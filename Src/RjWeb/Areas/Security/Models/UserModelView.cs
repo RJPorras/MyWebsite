5c715036bd62c213b100a23feaa384997b0508dc
RJPorras <rjporras28@gmail.com>
2017-02-21 15:55:32 -0800

    Init

---
+++ Src/RjWeb/Areas/Security/Models/UserModelView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RjWeb.Areas.Security.Models
+{
+    public class UserModelView
+    {
+        public Guid Id { get; set; }
+        [Required]
+        [MinLength(5,ErrorMessage="KULANG!")]
+        [MaxLength(10, ErrorMessage= "SOBRA!")]
+        public string FirstName{ get; set; }
+
+        [Required, Display(Name = "Family Name")]
+        public string LastName { get; set; }
+        public int? Age { get; set; }
+        //public string Birthday { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
